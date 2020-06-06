722b11273e3d661e131437d19726d0e1285ffb07
Ricardo Torres <rictor@cuhrt.com>
2020-06-06 10:10:32 -0500

    Agregand la opcion --html primera parte al script reservaciones.py

--- Sesion-03/reservaciones.py
+++ Sesion-03/reservaciones.py
@@ -1,3 +1,5 @@
+import click
+
 # 1. Generar la lista de conceptos en una lista de diccionarios
 # 2. Agregar el campo calculado de subtotal.
 # 3. Imprimir la tabla de la reservación.
@@ -42,16 +44,8 @@ def obtiene_conceptos():
     return conceptos
 
 
-def imprime_txt(conceptos):
+def imprime_txt(conceptos, total):
     """ Imprime en formato txt la lista de conceptos """
-    # Calcular el campo de subtotal y total
-    total = 0
-    for c in conceptos:  # c -> diccionario
-        c["SUBTOTAL"] = c["CANTIDAD"] * c["PRECIO"]
-
-    # Ordenamiento por nombre de concepto
-    conceptos.sort(key=lambda x: x["CONCEPTO"].lower())
-
     # Imprimiendo tabla
     linea = "-" * 70
     formato1 = "{:30} | {:8} | {:10} | {:10}"  # Encabezado
@@ -67,12 +61,48 @@ def imprime_txt(conceptos):
     print(linea)
     print(formato3.format("", "", "Total", total))
 
-def main():
-    """ Función principal del script """
+    
+def imprime_html(conceptos, total):
+    """ Imprime en formato HTML la lista de conceptos """
+    # Imprimiendo tabla
+    linea = "-" * 70
+    formato1 = "{:30} | {:8} | {:10} | {:10}"  # Encabezado
+    formato2 = "{:30} | {:8} | {:10.2f} | {:10.2f}"  # Para los conceptos
+    formato3 = "{:30} | {:8} | {:>10} | {:10.2f}"  # Para el total
+
+    print(linea)
+    print(formato1.format("CONCEPTO", "CANTIDAD", "PRECIO", "SUBTOTAL"))
+    print(linea)
+    for c in conceptos:
+        print(formato2.format(
+            c["CONCEPTO"], c["CANTIDAD"], c["PRECIO"], c["SUBTOTAL"]))
+    print(linea)
+    print(formato3.format("", "", "Total", total))
+
+
+@click.command()
+@click.option("--html", "es_html", is_flag=True,
+    help="Imprime el resultado en formato HTML")
+def main(es_html):
+    """ Imprime la lista de conceptos de una reservación """
+    
     # Llamamos a la función y guardamos el valor que regresa
     conceptos = obtiene_conceptos()
-    imprime_txt(conceptos)
+    # Calcular el campo de subtotal y total
+    total = 0
+    for c in conceptos:  # c -> diccionario
+        c["SUBTOTAL"] = c["CANTIDAD"] * c["PRECIO"]
 
-# Ejecuta sólo si es el programa principal
-if __name__ == "__main__":
+    # Ordenamiento por nombre de concepto
+    conceptos.sort(key=lambda x: x["CONCEPTO"].lower())
+    
+    if es_html:
+        imprime_html(conceptos, total)
+    else:
+        imprime_txt(conceptos, total)
+
+if __name__  == "__main__":
+    # Si __name__ es igual al valor "__main__" el script se ejecuta
+    # desde la línea de comando y tenemos que ejecutar la función
+    # main()
     main()
