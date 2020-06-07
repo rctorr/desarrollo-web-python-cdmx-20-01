924ba4dc7c6fa3204262673bfed7dd986c23bd23
Ricardo Torres <rictor@cuhrt.com>
2020-06-06 23:09:34 -0500

    Agregando archivos finales para la Sesion-03, Sesion-04 y Sesion-05, así como notas actualizadas

--- Sesion-04/reservaciones.py
+++ Sesion-04/reservaciones.py
@@ -1,50 +1,30 @@
 import click
+import csv
 
 # 1. Generar la lista de conceptos en una lista de diccionarios
 # 2. Agregar el campo calculado de subtotal.
 # 3. Imprimir la tabla de la reservación.
 
 
-def obtiene_conceptos():
-    """ Obtiene la lista de comceptos de una reservación """
-    conceptos = [
-        {
-            "CONCEPTO":"Habitación doble",
-            "CANTIDAD":1,
-            "PRECIO":15000.00
-        },
-        {
-            "CONCEPTO":"Transporte",
-            "CANTIDAD":3,
-            "PRECIO":3000.00
-        },
-        {
-            "CONCEPTO":"Reservación en evento",
-            "CANTIDAD":1,
-            "PRECIO":3999.99
-        }
-    ]
-
-    # Agregando un sólo concepto a la lista
-    concepto = {
-        "CONCEPTO":"Tour en lancha",
-        "CANTIDAD":2,
-        "PRECIO":2175.00
-    }
-    conceptos.append(concepto)
-
-    # Agregando otro concepto a la lista
-    concepto = {
-        "CONCEPTO":"alimentos y bebidas",
-        "CANTIDAD":1,
-        "PRECIO":5000.00
-    }
-    conceptos.append(concepto)
-    
-    return conceptos
+def obtiene_conceptos(nomarch):
+    """
+    Obtiene la lista de comceptos de una reservación desde nomarch
+    """
+    # Se lee todas las fila de un sólo golpe
+    with open(nomarch) as da:
+        da_csv = csv.DictReader(da)
+        encabezados = da_csv.fieldnames
+        conceptos = list(da_csv)
+        
+    # Se realiza conversión de tipos de datos
+    for concepto in conceptos:
+        concepto["CANTIDAD"] = int(concepto["CANTIDAD"])
+        concepto["PRECIO"] = float(concepto["PRECIO"])
+        
+    return encabezados, conceptos
 
 
-def imprime_txt(conceptos, total):
+def imprime_txt(conceptos, total, encabezados):
     """ Imprime en formato txt la lista de conceptos """
     # Imprimiendo tabla
     linea = "-" * 70
@@ -53,26 +33,24 @@ def imprime_txt(conceptos, total):
     formato3 = "{:30} | {:8} | {:>10} | {:10.2f}"  # Para el total
 
     print(linea)
-    print(formato1.format("CONCEPTO", "CANTIDAD", "PRECIO", "SUBTOTAL"))
+    print(formato1.format(*encabezados))
     print(linea)
     for c in conceptos:
-        print(formato2.format(
-            c["CONCEPTO"], c["CANTIDAD"], c["PRECIO"], c["SUBTOTAL"]))
+        print(formato2.format(*c.values()))
     print(linea)
     print(formato3.format("", "", "Total", total))
 
     
-def imprime_html(conceptos, total):
+def imprime_html(conceptos, total, encabezados):
     """ Imprime en formato HTML la lista de conceptos """
     # Imprimiendo tabla
     formato1 = "<tr><th>{}</th><th>{}</th><th>{}</th><th>{}</th></tr>"
     formato2 = "<tr><td>{}</td><td>{}</td><td>{}</td><td>{}</td></tr>"  # Para los conceptos
 
     print("<table>")
-    print(formato1.format("CONCEPTO", "CANTIDAD", "PRECIO", "SUBTOTAL"))
+    print( formato1.format(*encabezados) )
     for c in conceptos:
-        print(formato2.format(
-            c["CONCEPTO"], c["CANTIDAD"], c["PRECIO"], c["SUBTOTAL"]))
+        print( formato2.format(*c.values()) )
     print(formato2.format("", "", "Total", total))
     print("</table>")
 
@@ -82,21 +60,24 @@ def imprime_html(conceptos, total):
     help="Imprime el resultado en formato HTML")
 def main(es_html):
     """ Imprime la lista de conceptos de una reservación """
+    nomarch = "reservaciones.csv"
     
     # Llamamos a la función y guardamos el valor que regresa
-    conceptos = obtiene_conceptos()
-    # Calcular el campo de subtotal y total
+    encabezados, conceptos = obtiene_conceptos(nomarch)
+    # Se calcula y agrega la columna de subtotal y de paso el total
+    encabezados.append("SUBTOTAL")
     total = 0
     for c in conceptos:  # c -> diccionario
         c["SUBTOTAL"] = c["CANTIDAD"] * c["PRECIO"]
+        total += c["SUBTOTAL"]
 
     # Ordenamiento por nombre de concepto
-    conceptos.sort(key=lambda x: x["CONCEPTO"].lower())
+    conceptos.sort(key=lambda x: x["PRODUCTO"].lower())
     
     if es_html:
         imprime_html(conceptos, total)
     else:
-        imprime_txt(conceptos, total)
+        imprime_txt(conceptos, total, encabezados)
 
 if __name__  == "__main__":
     # Si __name__ es igual al valor "__main__" el script se ejecuta
