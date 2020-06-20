d1364f67f79d7a69897a5014fce201ef2f76adaa
Ricardo Torres <rictor@cuhrt.com>
2020-06-20 13:36:42 -0500

    Actualizando primer_spider.py para navegar en todas las paginas

--- Sesion-07/primer_spider.py
+++ Sesion-07/primer_spider.py
@@ -14,9 +14,15 @@ class CitasSpider(scrapy.Spider):
         # Vamos a iniciar la búsqueda de las citas
         # <div class="quote" ...>...</div>
         for cita in respuesta.css("div.quote"):
+            # Filtramos la citas
+            if "Albert" in cita.xpath("span/small/text()").get():
             # Regresar sólo una cita
-            yield {
-                "texto" : cita.css("span.text::text").get(),
-                "autor" : cita.xpath("span/small/text()").get(),
-            }
-            
+                yield {
+                    "texto" : cita.css("span.text::text").get(),
+                    "autor" : cita.xpath("span/small/text()").get(),
+                }
+                
+        # Navegando a la siguiente página
+        pag_sig = respuesta.css("li.next a::attr('href')").get()
+        if pag_sig is not None:
+            yield respuesta.follow(pag_sig, self.parse)
